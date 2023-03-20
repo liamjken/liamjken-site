1ba2fab60229c9843b2e3fe51b25819f0b101796
liamjken <liamjken@gmail.com>
2023-03-20 11:35:05 -0400

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<h1>Pages</h1>
+    <div v-if="pages.length">
+    <div v-for="page in pages" :key="page.id" class="page">
+    <h2>{{ page.title.rendered }}</h2>
+    </div></div>
+    <div v-else>
+        <p>loading pages...</p>
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            pages: []
+        }
+    },
+    mounted() {
+        fetch('https://www.liamjken.com/wp-json/wp/v2/pages')
+        .then(res => res.json())
+        .then(data => this.pages = data)
+        .catch(err => console.log(err.message))
+    }
+}
+</script>
+
+<style>
+</style>
